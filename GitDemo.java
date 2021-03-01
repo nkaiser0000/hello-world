5731ba81cffa4f1d703d637a2ce7179b9e66b165
nkaiser0000 <66803003+nkaiser0000@users.noreply.github.com>
2021-02-28 23:46:37 -0700

    Added first part changes

--- GitDemo.java
+++ GitDemo.java
@@ -4,7 +4,7 @@
 public class GitDemo {
     
     private double previousNumber;
-    
+    private double reverseNum;
     public GitDemo() {
         previousNumber = 1;
     }
@@ -14,7 +14,8 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        previousNumber += num;
+        return previousNumber;
     }
     
     /*****
@@ -45,7 +46,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
