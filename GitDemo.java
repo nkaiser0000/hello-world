8bc619c93d9bb06eddbd17f1b577a55b8ed50d2d
nkaiser0000 <66803003+nkaiser0000@users.noreply.github.com>
2021-02-28 23:48:42 -0700

    Second part changes

--- GitDemo.java
+++ GitDemo.java
@@ -25,11 +25,10 @@ public class GitDemo {
     public double findFactorial(int num) {
         double factorialNumber = 0;
         
-        while( num != 0 ) {
-            double temp = num % 10;
-            factorialNumber = factorialNumber * 10 + temp;
-            num /= 10;
+        for(int i = 1; i <= num; ++i){
+            factorialNumber = factorialNumber * i;
         }
+        
         
         return factorialNumber;
     }
@@ -42,10 +41,12 @@ public class GitDemo {
     public double reverseNumber(int num){
         double newNumber = 1;
         
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
-        }
         
+        while( num != 0 ) {
+            double temp = num % 10;
+            newNumber = newNumber * 10 + temp;
+            num /= 10;
+        }
         return newNumber;
     }
     
